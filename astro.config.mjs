e491064d0a77ffa2c6a09a14eca737d5ee1f1e5a
TeamMalcic <malcic.riederich@gmail.com>
2026-02-11 17:12:25 +0100

    fix github adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github-pages';
 
 export default defineConfig({
-  site: 'https://malcicriederich-bit.github.io/',
-  base: '/',
+  site: 'https://malcicriederich-bit.github.io',
   output: 'static',
   adapter: github(),
 });
